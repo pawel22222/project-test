13bff43db776d21268bc06a59041f38f025c7687
Pawel Sokolowski <pawelsokolowski22222@gmail.com>
2022-04-08 15:38:37 +0200

    feat: init reset_slots_pizza_form function

--- actions/actions.py
+++ actions/actions.py
@@ -1,12 +1,14 @@
 from typing import Text, List, Any, Dict
 
 from rasa_sdk import Tracker, FormValidationAction, Action
-from rasa_sdk.events import EventType
+from rasa_sdk.events import EventType, SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
-ALLOWED_PIZZA_SIZES = ["small", "medium", "large", "extra-large", "extra large", "s", "m", "l", "xl"]
+ALLOWED_PIZZA_SIZES = ["small", "medium", "large",
+                       "extra-large", "extra large", "s", "m", "l", "xl"]
 ALLOWED_PIZZA_TYPES = ["mozzarella", "fungi", "veggie", "pepperoni", "hawaii"]
+
 
 class ValidateSimplePizzaForm(FormValidationAction):
     def name(self) -> Text:
@@ -19,7 +21,6 @@ class ValidateSimplePizzaForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-
         if slot_value.lower() not in ALLOWED_PIZZA_SIZES:
             dispatcher.utter_message(text=f"We only accept pizza sizes: s/m/l/xl.")
             return {"pizza_size": None}
@@ -33,9 +34,12 @@ class ValidateSimplePizzaForm(FormValidationAction):
         traclargeker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-
         if slot_value not in ALLOWED_PIZZA_TYPES:
             dispatcher.utter_message(text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
             return {"pizza_type": None}
         dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
         return {"pizza_type": slot_value}
+
+    def reset_slots_pizza_form(self, dispatcher, tracker, domain):
+        # SlotSet("pizza_size", None)
+        return 0
