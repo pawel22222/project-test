f234025c7b88351249a25d5e5b9be12fbbd3598a
Pawel Sokolowski <pawelsokolowski22222@gmail.com>
2022-04-12 13:13:29 +0200

    feat: rich response with buttons - utter_ask_pizza feat: intents give_time and custom action ShowTime readme: update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # project-test
+
+## Get started 
+
+- install python 3.7.0
+- pip install -r requirements.txt
+- python -m venv /path/to/new/virtual/environment
+- path_to_virtualenv/Scripts/activate
+- rasa run activate
+- rasa train
+- rasa shell
--- actions/actions.py
+++ actions/actions.py
@@ -1,9 +1,11 @@
 from typing import Text, List, Any, Dict
+import datetime as dt
 
 from rasa_sdk import Tracker, FormValidationAction, Action
 from rasa_sdk.events import EventType, SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
+from sqlalchemy import null
 
 ALLOWED_PIZZA_SIZES = ["small", "medium", "large",
                        "extra-large", "extra large", "s", "m", "l", "xl"]
@@ -19,27 +21,52 @@ class ValidateSimplePizzaForm(FormValidationAction):
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
-        domain: DomainDict,
+        domain: Dict[Text, Any]
     ) -> Dict[Text, Any]:
         if slot_value.lower() not in ALLOWED_PIZZA_SIZES:
-            dispatcher.utter_message(text=f"We only accept pizza sizes: s/m/l/xl.")
+            dispatcher.utter_message(
+                text=f"We only accept pizza sizes: s/m/l/xl.")
             return {"pizza_size": None}
-        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
+        dispatcher.utter_message(
+            text=f"OK! You want to have a {slot_value} pizza.")
         return {"pizza_size": slot_value}
 
     def validate_pizza_type(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
-        traclargeker: Tracker,
-        domain: DomainDict,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
     ) -> Dict[Text, Any]:
         if slot_value not in ALLOWED_PIZZA_TYPES:
-            dispatcher.utter_message(text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
+            dispatcher.utter_message(
+                text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
             return {"pizza_type": None}
-        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
+        dispatcher.utter_message(
+            text=f"OK! You want to have a {slot_value} pizza.")
         return {"pizza_type": slot_value}
 
-    def reset_slots_pizza_form(self, dispatcher, tracker, domain):
-        # SlotSet("pizza_size", None)
-        return 0
+
+class ShowTime(Action):
+    def name(self) -> Text:
+        return "action_show_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=f"{dt.datetime.now()}")
+
+        return []
+
+
+# Not working
+class ResetSlots(Action):
+    def name(self) -> Text:
+        return "action_reset_slots"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return SlotSet("pizza_size", null)
+
