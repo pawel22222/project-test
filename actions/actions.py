e44cc67bfad54377f16e30f1c221bfc1d8ab30c9
Pawel Sokolowski <pawelsokolowski22222@gmail.com>
2022-04-19 16:02:17 +0200

    refactor

--- actions/actions.py
+++ actions/actions.py
@@ -6,47 +6,7 @@ from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
-ALLOWED_PIZZA_SIZES = ["small", "medium", "large",
-                       "extra-large", "extra large", "s", "m", "l", "xl"]
-ALLOWED_PIZZA_TYPES = ["mozzarella", "fungi", "veggie", "pepperoni", "hawaii"]
-
 ALLOWED_DISHES = ["soup", "pizza", "hamburger", "kebab", "sushi", "cola"]
-
-
-class ValidateSimplePizzaForm(FormValidationAction):
-    def name(self) -> Text:
-        return "validate_simple_pizza_form"
-
-    def validate_pizza_size(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ) -> Dict[Text, Any]:
-        if slot_value.lower() not in ALLOWED_PIZZA_SIZES:
-            dispatcher.utter_message(
-                text=f"We only accept pizza sizes: s/m/l/xl.")
-            return {"pizza_size": None}
-        dispatcher.utter_message(
-            text=f"OK! You want to have a {slot_value} pizza.")
-        return {"pizza_size": slot_value}
-
-    def validate_pizza_type(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ) -> Dict[Text, Any]:
-        if slot_value not in ALLOWED_PIZZA_TYPES:
-            dispatcher.utter_message(
-                text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
-            return {"pizza_type": None}
-        dispatcher.utter_message(
-            text=f"OK! You want to have a {slot_value} pizza.")
-        return {"pizza_type": slot_value}
-
 
 class ShowTime(Action):
     def name(self) -> Text:
@@ -60,9 +20,9 @@ class ShowTime(Action):
         return []
 
 
-class ValidateBookingForm(FormValidationAction):
+class ValidateBookingTableForm(FormValidationAction):
     def name(self) -> Text:
-        return "validate_restaurant_booking_form"
+        return "validate_booking_table_form"
 
     def validate_dishes(
         self,
@@ -88,17 +48,16 @@ class ValidateBookingForm(FormValidationAction):
         tracker: Tracker,
         domain: Dict[Text, Any]
     ) -> Dict[Text, Any]:
-        if int(slot_value) >= 20:
-            dispatcher.utter_message(
-                text=f"I don't understand. can you choose number from 1 to 6?")
+        if int(slot_value) <= 15:
+            dispatcher.utter_message(text=f"OK! You want table for {slot_value} people.")
+            return {"num_people": slot_value}
+        else:
+            dispatcher.utter_message(text=f"I don't understand. can you choose number from 1 to 15?")
             return {"num_people": None}
-        dispatcher.utter_message(
-            text=f"OK! You want table for {slot_value} people.")
-        return {"num_people": slot_value}
 
-class SubmitBookingForm(Action):
+class SubmitBookingTableForm(Action):
     def name(self) -> Text:
-        return "submit_booking_form"
+        return "submit_booking_table_form"
 
     def run(
         self,
@@ -110,5 +69,7 @@ class SubmitBookingForm(Action):
         if tracker.get_slot('outdoor_seating'): table_type = 'outdoor'
         else: table_type = 'indoor'
 
+        dishes = ', '.join(tracker.get_slot('dishes'))
+
         dispatcher.utter_message(
-            text=f"OK! You want {table_type} table for {tracker.get_slot('num_people')} people.")
+            text=f"OK! You want {table_type} table for {tracker.get_slot('num_people')} people and you ordered {dishes}")
